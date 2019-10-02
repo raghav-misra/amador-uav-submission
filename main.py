6a6c2663754bfd13be03d5e01b74d74f99549389
Raghav Misra <raghav.m2014@gmail.com>
2019-10-02 12:48:37 -0700

    Started I guess...

---
+++ main.py
@@ -0,0 +1,7 @@
+def get_input():
+    lines_raw = []
+    with open("input.in", "r") as input_file:
+        lines_raw = input_file.readlines()
+        print(lines_raw)
+
+raw_input_data = get_input()
